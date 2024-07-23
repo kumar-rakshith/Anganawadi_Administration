d9515541929d32559a2ef8b407a4ce50e99ca5d1
kumar-rakshith <rakshithkumar.thokur@gmail.com>
2024-07-23 17:34:01 +0530

    file addition

---
+++ Controller/application.php
@@ -0,0 +1,30 @@
+<?php
+define('DIR','../');
+require_once DIR . 'config.php';
+
+$control = new Controller(); 
+$admin = new Admin();
+
+
+if(isset($_POST['add']))
+{
+	
+	$sid=$_POST['s_id'];
+	$name=$_POST['name'];
+	
+	$pnumber=$_POST['pnumber'];
+	$anumber=$_POST['anumber'];
+	$rnumber=$_POST['rnumber'];
+	
+	$vid=$_POST['vid'];
+	$caste=$_POST['caste'];
+	$fname=$_POST['fname'];
+	
+	$stmt=$admin->cud("INSERT INTO `tbl_application`(`s_id`, `name`, `pnumber`, `anumber`,`rnumber`, `vid`,`caste`,`fname`) VALUES ('".$s_id."','".$name."','".$fname."','".$anumber."','".$rnumber."','".$vid."','".$caste."','".$pnumber."')","saved");
+
+	echo "<script>alert('data saved')</script>";
+	    $admin->redirect('../Resident/index');
+
+}
+
+?>
